4279471cb708dc390c24107a414827d513b5f57c
sleshJdev <slesh.eugene93@gmail.com>
2016-04-19 22:56:43 +0300

    improved the TokenService and test for it

--- imaster-server/src/main/scala/by/slesh/bntu/imaster/security/TokenService.scala
+++ imaster-server/src/main/scala/by/slesh/bntu/imaster/security/TokenService.scala
@@ -4,20 +4,27 @@ import authentikat.jwt._
 import org.slf4j.LoggerFactory
 
 /**
- * @author yauheni.putsykovich
- */
+  * @author yauheni.putsykovich
+  */
 object TokenService {
   private val LOGGER = LoggerFactory.getLogger(getClass)
 
-  def algorithm = "HS256"
-  def secretKey = "secretkey"
+  var secretKey = "secretkey"
+  var algorithm = "HS384"
 
   def createToken(claim: Map[String, String]): String = {
     val header = JwtHeader(algorithm)
     val claimsSet = JwtClaimsSet(claim)
-    val jwt: String = JsonWebToken(header, claimsSet, secretKey)
-    LOGGER.info("has been created jwt-token: {} with alg: {}, claims: {} and key: {}", jwt, algorithm, claim, secretKey)
-    jwt
+    JsonWebToken(header, claimsSet, secretKey)
+  }
+
+  def parseToken(jwt: String): Option[Map[String, String]] = {
+    jwt match {
+      case JsonWebToken(header, claimsSet, signature) =>
+        claimsSet.asSimpleMap.toOption
+      case x =>
+        None
+    }
   }
 
   def validate(jwt: String) = JsonWebToken.validate(jwt, secretKey)
--- imaster-server/src/test/scala/by/slesh/bntu/imaster/security/TokenServiceTests.scala
+++ imaster-server/src/test/scala/by/slesh/bntu/imaster/security/TokenServiceTests.scala
@@ -3,19 +3,27 @@ package by.slesh.bntu.imaster.security
 import org.scalatest._
 
 /**
- * @author yauheni.putsykovich
- */
-class TokenServiceTests extends FlatSpec {
-  val CLAIM = Map("Hey" -> "foo")
+  * @author yauheni.putsykovich
+  */
+class TokenServiceTests extends FlatSpec with Matchers {
+  val CLAIMS = Map("Hey" -> "foo")
 
   "A TokenService" should "create valid jwt-token" in {
-    val token = TokenService.createToken(CLAIM)
+    val token = TokenService.createToken(CLAIMS)
     assert(TokenService.validate(token))
   }
 
-  ignore should "be equal a expected token" in {
-    val expectedToken = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJIZXkiOiJmb28ifQ==.e89b48f1e2016b78b805c1430d5a139e62bba9237ff3e34bad56dae6499b2647"
-    val token = TokenService.createToken(CLAIM)
-    assertResult(expectedToken)(token)
+  it should "be equal a expected token" in {
+    //    val expectedToken = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJIZXkiOiJmb28ifQ==.e89b48f1e2016b78b805c1430d5a139e62bba9237ff3e34bad56dae6499b2647"
+    val expectedToken = "eyJhbGciOiJIUzM4NCIsInR5cCI6IkpXVCJ9.eyJIZXkiOiJmb28ifQ.mN_sJOimR3xPJEu5lAN9YHu_wlDuuIStCyJ4jtMVEICr9TLfOweNecAu76Pk1Xxr"
+    val actualToken = TokenService.createToken(CLAIMS)
+    assertResult(expectedToken)(actualToken)
+  }
+
+  it should "return jwt-token which contains same data as source claims" in {
+    val jwt = TokenService.createToken(CLAIMS)
+    val claims: Option[Map[String, String]] = TokenService.parseToken(jwt)
+    claims.isDefined should be(true)
+    claims.get should equal(CLAIMS)
   }
 }
