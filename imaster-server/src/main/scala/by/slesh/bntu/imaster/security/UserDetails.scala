b4df2d03d1c148bab9eb6ac6b1e4b752145d8870
sleshJdev <slesh.eugene93@gmail.com>
2016-04-23 02:05:50 +0300

    created new structure for project

--- imaster-server/src/main/scala/by/slesh/bntu/imaster/security/UserDetails.scala
+++
@@ -1,7 +0,0 @@
-package by.slesh.bntu.imaster.security
-
-/**
-  * @author slesh
-  */
-case class UserDetails(username: String,
-                       password: String)
--- imaster-server/src/main/scala/by/slesh/bntu/imaster/web/model/Models.scala
+++
@@ -1,7 +0,0 @@
-package by.slesh.bntu.imaster.web.model
-
-/**
- * @author yauheni.putsykovich
- */
-case class Account(username: String,
-                   password: String)
