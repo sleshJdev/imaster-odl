cbcc1e44320a807e2ec3d5013014120e96e7e47f
yauheni.putsykovich <yauheni.putsykovich@itechart-group.com>
2016-05-29 02:46:20 +0300

    add foreign key constrains for user_role table

--- src/imaster-server/src/main/scala/by/slesh/bntu/imaster/persistence/UserRoles.scala
+++ src/imaster-server/src/main/scala/by/slesh/bntu/imaster/persistence/UserRoles.scala
@@ -10,6 +10,9 @@ class UserRoles(tag: Tag) extends Table[(Int, Int)](tag, "user_role") {
 
   def roleId = column[Int]("roleId")
 
+  def user = foreignKey("fk__user_role__user_id__user_id", userId, User.models)(_.id)
+  def role = foreignKey("fk__user_role__role_id__role_id", roleId, Role.models)(_.id)
+
   def id = primaryKey("pk_user_x_role", (userId, roleId))
 
   override def * = (userId, roleId)
